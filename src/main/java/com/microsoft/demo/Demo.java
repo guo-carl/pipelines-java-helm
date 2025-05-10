f4d9a9f2041a8ad5bd40853a68995c07b42856be
Guo Yang <42885904+guo-carl@users.noreply.github.com>
2025-05-10 23:53:55 +0800

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -2,11 +2,33 @@ package com.microsoft.demo;
 
 public class Demo {
     public void DoSomething(boolean flag){
-        if(flag){
+        if(flag==true){
             System.out.println("I am covered");
             return;
         }
-
+            int a=0;
+            if (a == 0) {
+                a = 1;
+            }else if (a == 1) {
+                a = 2;
+            } else if (a==2) {
+                a = 3;
+            } else if (a == 3) {
+                a = 4;
+            } else if (a == 4) {
+                a = 5;
+            } else if (a == 5) {
+                a = 6;
+            } else if (a == 6) {
+                a = 7;
+            } else if (a == 7) {
+                a = 8;
+            } else if (a == 8) {
+                a = 9;
+            } else if (a == 9) {
+                a = 10;
+            }
+            println(a);
         System.out.println("I am not covered");
     }
 }
